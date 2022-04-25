17ecde49e80abbf58a92231c343d35fb3ec88911
kieranwoodrow <kwoodrow@jhb.dvt.co.za>
2022-04-25 21:42:58 +0200

    Created weather view controller

--- Weather/Weather/Features/Landing Weather Page /ViewController/WeatherViewController.swift
+++ Weather/Weather/Features/Landing Weather Page /ViewController/WeatherViewController.swift
@@ -1,1 +1,69 @@
-// Test
+//
+//  WeatherViewController.swift
+//  Weather
+//
+//  Created by Kieran Woodrow on 2022/04/21.
+//
+
+import UIKit
+import Foundation
+
+class WeatherViewController: UIViewController {
+    
+    private lazy var weatherViewModel = WeatherViewModel(repository: WeatherRepository(),
+                                                         delegate: self)
+    
+    @IBOutlet weak private var tempValue: UILabel!
+    @IBOutlet weak private var tempCondition: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        weatherViewModel.getWeather()
+//        setTableView()
+        tempValue.text = String(weatherViewModel.currentTemp)
+        tempCondition.text = weatherViewModel.condition
+        
+        
+    }
+    
+//    private func setTableView() {
+//        testtableview.delegate = self
+//        testtableview.dataSource = self
+//    }
+    
+}
+
+//extension WeatherViewController: UITableViewDelegate, UITableViewDataSource {
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        1
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//        guard let cell = testtableview.dequeueReusableCell(withIdentifier: "test", for: indexPath) as? UITableViewCell
+//        else {
+//            return UITableViewCell()
+//        }
+//
+//        cell.textLabel!.text = String(weatherViewModel.currentTemp)
+//        print(weatherViewModel.currentTemp)
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 400
+//    }
+//}
+
+extension WeatherViewController:  ViewModelDelegate {
+    
+    func reloadView() {
+        //self.testtableview.reloadData()
+    }
+    
+    func show(error: CustomError) {
+        
+    }
+}
+
--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -1,63 +1,63 @@
+////
+////  ViewController.swift
+////  Weather
+////
+////  Created by Kieran Woodrow on 2022/04/21.
+////
 //
-//  ViewController.swift
-//  Weather
+//import UIKit
+//import Foundation
 //
-//  Created by Kieran Woodrow on 2022/04/21.
+//class ViewController: UIViewController {
+//    
+//    private lazy var weatherViewModel = WeatherViewModel(repository: WeatherRepository(),
+//                                                         delegate: self)
+//    @IBOutlet weak var testtableview: UITableView!
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        weatherViewModel.getWeather()
+//        setTableView()
+//        
+//    }
+//    
+//    private func setTableView() {
+//        testtableview.delegate = self
+//        testtableview.dataSource = self
+//    }
+//    
+//}
 //
-
-import UIKit
-import Foundation
-
-class ViewController: UIViewController {
-    
-    private lazy var weatherViewModel = WeatherViewModel(repository: WeatherRepository(),
-                                                         delegate: self)
-    @IBOutlet weak var testtableview: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        weatherViewModel.getWeather()
-        setTableView()
-        
-    }
-    
-    private func setTableView() {
-        testtableview.delegate = self
-        testtableview.dataSource = self
-    }
-    
-}
-
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = testtableview.dequeueReusableCell(withIdentifier: "test", for: indexPath) as? UITableViewCell
-        else {
-            return UITableViewCell()
-        }
-        
-        cell.textLabel!.text = String(weatherViewModel.currentTemp)
-        print(weatherViewModel.currentTemp)
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 400
-    }
-}
-
-extension ViewController:  ViewModelDelegate {
-    
-    func reloadView() {
-        self.testtableview.reloadData()
-    }
-    
-    func show(error: CustomError) {
-        
-    }
-}
+//extension ViewController: UITableViewDelegate, UITableViewDataSource {
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        1
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        
+//        guard let cell = testtableview.dequeueReusableCell(withIdentifier: "test", for: indexPath) as? UITableViewCell
+//        else {
+//            return UITableViewCell()
+//        }
+//        
+//        cell.textLabel!.text = String(weatherViewModel.currentTemp)
+//        print(weatherViewModel.currentTemp)
+//        return cell
+//    }
+//    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 400
+//    }
+//}
+//
+//extension ViewController:  ViewModelDelegate {
+//    
+//    func reloadView() {
+//        self.testtableview.reloadData()
+//    }
+//    
+//    func show(error: CustomError) {
+//        
+//    }
+//}
