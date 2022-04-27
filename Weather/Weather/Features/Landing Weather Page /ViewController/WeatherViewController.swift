6ca6ca9e303d9c5fc785464e449853235be47f5c
kieranwoodrow <kwoodrow@jhb.dvt.co.za>
2022-04-27 22:43:26 +0200

    Added core location features
    -Added core location object and delegate

--- Weather/Weather/Features/Landing Weather Page /ViewController/WeatherViewController.swift
+++ Weather/Weather/Features/Landing Weather Page /ViewController/WeatherViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Foundation
+import CoreLocation
 
 class WeatherViewController: UIViewController {
     
@@ -17,6 +18,7 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak private var currentTemp: UILabel!
     @IBOutlet weak private var maxTemp: UILabel!
     @IBOutlet weak private var forcastedTableView: UITableView!
+    private let locationManager = CLLocationManager()
     
     private lazy var weatherViewModel = WeatherViewModel(repository: WeatherRepository(),
                                                          delegate: self)
@@ -36,6 +38,10 @@ class WeatherViewController: UIViewController {
         weatherViewModel.weather()
     }
     
+    private func setCoreLocation() {
+        locationManager.delegate = self
+    }
+    
     private func setLabels() {
         tempValue.text = String(weatherViewModel.currentTemp)
         tempCondition.text = weatherViewModel.condition.uppercased()
@@ -45,9 +51,35 @@ class WeatherViewController: UIViewController {
     }
     
     private func setWeatherViewController() {
+        setCoreLocation()
         setTableView()
         setWeatherViewModel()
         currentTempView.addBorder(side: .bottom, color: .white, width: 0.5)
+    }
+    
+    func retriveCurrentLocation(){
+        let status = locationManager.authorizationStatus
+
+        if(status == .denied || status == .restricted || !CLLocationManager.locationServicesEnabled()){
+            // show alert to user telling them they need to allow location data to use some feature of your app
+            return
+        }
+
+        // if haven't show location permission dialog before, show it to user
+        if(status == .notDetermined){
+            locationManager.requestWhenInUseAuthorization()
+
+            // if you want the app to retrieve location data even in background, use requestAlwaysAuthorization
+            // locationManager.requestAlwaysAuthorization()
+            return
+        }
+        
+        // at this point the authorization status is authorized
+        // request location data once
+        locationManager.requestLocation()
+      
+        // start monitoring location data and get notified whenever there is change in location data / every few seconds, until stopUpdatingLocation() is called
+        // locationManager.startUpdatingLocation()
     }
 }
 
@@ -84,3 +116,26 @@ extension WeatherViewController:  ViewModelDelegate {
         
     }
 }
+    
+extension WeatherViewController : CLLocationManagerDelegate {
+    // called when the authorization status is changed for the core location permission
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("location manager authorization status changed")
+        
+        switch status {
+        case .authorizedAlways:
+            print("user allow app to get location data when app is active or in background")
+        case .authorizedWhenInUse:
+            print("user allow app to get location data only when app is active")
+        case .denied:
+            print("user tap 'disallow' on the permission dialog, cant get location data")
+        case .restricted:
+            print("parental control setting disallow location data")
+        case .notDetermined:
+            print("the location permission dialog haven't shown before, user haven't tap allow/disallow")
+        @unknown default:
+            <#fatalError()#>
+        }
+    }
+}
+
